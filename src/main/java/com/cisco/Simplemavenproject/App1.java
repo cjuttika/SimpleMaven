ac972aae44b6875b89173a092988359e52ff874b
cjuttika <chetanasai2@gmail.com>
2024-02-29 12:00:55 +0530

    Added App1.java

---
+++ src/main/java/com/cisco/Simplemavenproject/App1.java
@@ -0,0 +1,12 @@
+package com.cisco.Simplemavenproject;
+
+public class App1 {
+	
+
+	    public static void main( String[] args )
+	    {
+	        System.out.println( "Welcome to the world of the jenkins !" );
+	        System.out.println("Simple project for jenkins");
+	    }
+	}
+
